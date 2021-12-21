e1ee7b5b0aa329326fab03b3516804d0293a221f
fernando <ferrafterminador@gmail.com>
2021-12-21 09:03:16 -0600

    creando app de tienda

---
+++ Tienda/src/js/main.js
@@ -0,0 +1,6 @@
+const buscar = () =>{
+    console.log('hola')
+}
+document.getElementById('search').addEventListener('keyup', buscar)
+var algo =document.getElementById('search')
+console.log(algo)
