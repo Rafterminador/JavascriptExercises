73ac281e4554a302e984216c824b697916fcfdff
fernando <ferrafterminador@gmail.com>
2021-12-29 12:40:40 -0600

    tienda final con función búsqueda

--- Tienda/src/js/main.js
+++ Tienda/src/js/main.js
@@ -24,42 +24,35 @@ const starRating = (number) => {
     return classRating
 }
 
-class Datos{
-    static traerLibros(){
-        let libros
-        localStorage.getItem('tienda') === null ? libros = [] : libros = JSON.parse(localStorage.getItem('tienda'))
-        return libros
-    }
-    static agregarLibro(libro){
-        const libros = Datos.traerLibros()
-        libros.push(libro)
-        localStorage.setItem('libros', JSON.stringify(libros))
-    }
-    static removerLibro(isbn){
-        const libros = Datos.traerLibros()
-        libros.forEach( (libro, index) => {
-            if(libro.isbn === isbn){
-                libros.splice(index, 1)
-            }
-        })
-        localStorage.setItem('libros', JSON.stringify(libros))
-    }
-}
 class UI{
+    products = []
     static showOnInterface(element){
         const starTotal = 5
         let rate = starRating(element.rating.rate)
         const content = document.querySelector('.row')
         const mainDiv = document.createElement('div')
+        let subTitle = element.title
+        let spaceCount = (subTitle.split(" ").length - 1);
+        if(spaceCount >= 10){//esto para titulos largos
+            let newTitleArray = []
+            subTitle = subTitle.split(" ")
+            for(let i = 0; i<4; i++){
+                newTitleArray.push(subTitle[i])
+            }
+            subTitle = newTitleArray.join(' ')
+        }else{
+            subTitle = element.title
+        }
+
         mainDiv.className = 'col s6 l3'
         mainDiv.innerHTML = `
-            <div class="card">
+            <div class="card medium">
                 <div class="card-image waves-effect waves-block waves-light">
                     <img class="activator" src=${element.image}>  
                 </div>
                 <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">${element.title}<i class="material-icons right">more_vert</i></span>
-                    <p>Price: $${element.price}</p>
+                    <span class="card-title activator grey-text text-darken-4">${subTitle}<i class="material-icons right">more_vert</i></span>
+                    <p><strong>Price: $${element.price}</strong></p>
                 </div>
                 <div class="card-reveal">
                     <span class="card-title grey-text text-darken-4">${element.title}<i class="material-icons right">close</i></span>
@@ -77,38 +70,21 @@ class UI{
         `
         content.appendChild(mainDiv)
     }
-}
-const buscar = () =>{
-    const libros = Datos.traerLibros()
-    console.log('hola' + libros)
-    let example = {
-        "id": 1,
-        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        "price": 109.95,
-        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        "rating": {
-        "rate": 3.9,
-        "count": 120
-        }
+    static asign = (products) =>{
+        this.products = products
     }
-    UI.showOnInterface(example)
-    let example2 = {
-        "id": 2,
-        "title": "Mens Casual Premium Slim Fit T-Shirts ",
-        "price": 22.3,
-        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-        "category": "men's clothing",
-        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-        "rating": {
-        "rate": 2.4,
-        "count": 259
-        }
-        }
-        UI.showOnInterface(example2)
+    static search = (e) =>{
+        const content = document.querySelector('.row')
+        content.innerHTML = ''
+        var texto = e.target.value.toLowerCase()
+        this.products.forEach((element) => {
+            if(element.title.indexOf(texto) != -1 || element.category.indexOf(texto) != -1 || element.description.indexOf(texto) != -1){
+                UI.showOnInterface(element)
+            }
+        })
+    }
 }
-document.getElementById('search').addEventListener('keyup', buscar)
+document.getElementById('search').addEventListener('keyup', UI.search)
 const fetchStore = async () => {
     try {
       const res = await fetch(
@@ -128,6 +104,7 @@ const fetchStore = async () => {
         UI.showOnInterface(element)
       })
       //console.log(products[0])
+      UI.asign(products)
   }
   document.addEventListener('DOMContentLoaded', fetchProducts)
   
