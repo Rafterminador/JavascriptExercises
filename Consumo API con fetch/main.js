4e9e8b608290437b1124fe400f31fd8c99d6af2b
fernando <ferrafterminador@gmail.com>
2022-05-28 09:34:43 -0600

    usando query parameters

--- Consumo API con fetch/main.js
+++ Consumo API con fetch/main.js
@@ -1,11 +1,15 @@
-const API_URL = 'https://api.thecatapi.com/v1/images/search'
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&page=2'
 
 const reload = async () => {
     const res = await fetch(API_URL)
     const data = await res.json()
-    const img = document.getElementById('img-gato')
-    img.src = data[0].url
-    console.log(data[0].url, data[0].width, data[0].height)
+    console.log(data)
+    const img1 = document.getElementById('img-gato1')
+    const img2 = document.getElementById('img-gato2')
+    const img3 = document.getElementById('img-gato3')
+    img1.src = data[0].url
+    img2.src = data[1].url
+    img3.src = data[2].url
 }
 document.getElementById('recargar').addEventListener('click', reload)
 document.addEventListener('DOMContentLoaded', reload)
