b651b87184d9f4729ee8b78404c89ee3420c80cf
fernando <ferrafterminador@gmail.com>
2022-06-07 15:09:17 -0600

    De fetch a Axios

--- Consumo API con fetch/main.js
+++ Consumo API con fetch/main.js
@@ -1,8 +1,10 @@
-const API_URL = 'https://api.thecatapi.com/v1/'
-//const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=2&page=2&api_key=
 const API_KEY = '44f411a2-b8c1-4c31-b2c1-e9daab99861d'
 const spanError = document.getElementById('error')
 const saveButtons = document.getElementsByClassName('BotonFav')
+const api = axios.create({
+    baseURL: 'https://api.thecatapi.com/v1',
+})
+api.defaults.headers.common['x-api-key'] = API_KEY;
 const HTTPCodes = {
     200: 'OK',
     201: 'CREATED',
@@ -16,9 +18,13 @@ const HTTPCodes = {
 }
 
 const loadRandomCats = async () => {
-    const res = await fetch(API_URL + 'images/search?limit=2&page=2')
-    const data = await res.json()
-    if (res.ok) {
+    const { data, status } = await api.get('/images/search', {
+        params: {
+            limit: 2,
+            page: 2
+        }
+    })
+    if (status === 200) {
         const img1 = document.getElementById('img-gato1')
         const img2 = document.getElementById('img-gato2')
         img1.src = data[0].url
@@ -27,26 +33,14 @@ const loadRandomCats = async () => {
         img2.className = ""
         img1.classList.add(data[0].id)
         img2.classList.add(data[1].id)
-        //element.classList.remove('class-3');
     } else {
         spanError.innerHTML = "hubo error al cargar la API"
     }
-
-
 }
 const loadFavoritesCats = async () => {
     document.getElementById('GatosFavoritos').innerHTML = ""
-    let headersList = {
-        "Content-Type": "application/json",
-        "x-api-key": API_KEY,
-    }
-    const res = await fetch(API_URL + 'favourites', {
-        headers: headersList
-    })
-    if (!res.ok) {
-        spanError.innerHTML = HTTPCodes[res.status]
-    } else {
-        const data = await res.json()
+    const { data, status } = await api.get('/favourites')
+    if (status === 200) {
         const FavoritesCats = document.getElementById('GatosFavoritos')
         data.forEach((element) => {
             let article = document.createElement('article')
@@ -65,76 +59,39 @@ const loadFavoritesCats = async () => {
         for (let i = 0; i < removeButtons.length; i++) {
             removeButtons[i].addEventListener('click', removeFavoritesCats)
         }
+    } else {
+        spanError.innerHTML = HTTPCodes[res.status]
     }
 }
 
 const saveFavoritesCats = async (e) => {
     let article = e.target.parentNode
     let id = article.firstElementChild.className
-    let headersList = {
-        "Content-Type": "application/json",
-        "x-api-key": API_KEY,
-    }
-
-    let bodyContent = JSON.stringify({
+    const { data, status } = await api.post('/favourites', {
         image_id: id
-    });
-    const res = await fetch(API_URL + 'favourites', {
-        method: 'POST',
-        headers: headersList,
-        body: bodyContent,
     })
-    const data = await res.json()
     loadFavoritesCats()
     loadRandomCats()
 }
 const removeFavoritesCats = async (e) => {
     const article = e.target.parentNode
     let id = article.firstElementChild.className
-    let headersList = {
-        "Content-Type": "application/json",
-        "x-api-key": API_KEY,
-    }
-    const res = await fetch(API_URL + 'favourites/' + id, {
-        method: 'DELETE',
-        headers: headersList,
-    })
-    const data = await res.json()
+    const { data, status } = await api.delete(`/favourites/${id}`)
     loadFavoritesCats()
-    //location.reload();
 }
 const uploadGatoPhoto = async () => {
     const form = document.getElementById('uploadingForm')
     const formData = new FormData(form)//recibe form como atributo por lo que recibe todos los valores de los input
-    console.log(formData.get('file'))
-    let headersList = {
-        //"Content-Type": "multipart/form-data",
-        "x-api-key": API_KEY,
-    }
-    const res = await fetch(API_URL + 'images/upload', {
-        method: 'POST',
-        headers: headersList,
-        body: formData,
-    })
-    if (!res.ok) {
-        spanError.innerHTML = HTTPCodes[res.status]
-    } else {
-        let data = await res.json();
-        console.log("la data es ", data)
+    //console.log(formData.get('file'))
+    const { data, status } = await api.post('/images/upload', formData)
+    if (status === 201) {
         let id = data.id
-        let headersList = {
-            "Content-Type": "application/json",
-            "x-api-key": API_KEY,
-        }
-        let bodyContent = JSON.stringify({
+        let { data2, status2 } = await api.post('/favourites', {
             image_id: id
-        });
-        const res2 = await fetch(API_URL + 'favourites', {
-            method: 'POST',
-            headers: headersList,
-            body: bodyContent,
         })
         loadFavoritesCats()
+    } else {
+        spanError.innerHTML = HTTPCodes[res.status]
     }
 }
 document.getElementById('recargar').addEventListener('click', loadRandomCats)
